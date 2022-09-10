8d672eb8121884e6a4b90b81a767283ade96c05b
paalss <paalss94@gmail.com>
2022-09-10 21:19:41 +0200

    fetch text using selector and write to file

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const puppeteer = require("puppeteer");
-const fs = require('fs/promises')
+const fs = require("fs/promises");
 
 const start = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto("https://learnwebcode.github.io/practice-requests");
-  
-  const names = ['red', 'orange', 'yellow'];
-  await fs.writeFile("names.txt", names.join("\r\n"))
+
+  const names = await page.evaluate(() => {
+    // client side js
+    return Array.from(document.querySelectorAll(".info strong")).map(
+      (x) => x.textContent
+    );
+  });
+
+  await fs.writeFile("names.txt", names.join("\r\n"));
 
   await browser.close();
 };
