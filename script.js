c180e552ac35f5f5f11a63056219616ff7576f8f
paalss <paalss94@gmail.com>
2022-09-17 23:29:30 +0200

    find and navigate to job ad (using $$eval)

--- script.js
+++ script.js
@@ -2,47 +2,49 @@ import puppeteer from "puppeteer";
 import { createFile } from "./import/createFile.js";
 
 const start = async () => {
-  console.log('start');
+  console.log("start");
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   const joblist = "https://jobb.tu.no/sok?industries[]=IT&q=frontend";
   await page.goto(joblist);
   await page.screenshot({ path: "screenshots/joblist.png" });
 
-  console.group('jobAdLinksEVALUATE');
-  const jobAdLinksEVALUATE = await page.evaluate(() => {
-    // evaluate er generic for generell, catch all, client side js
-    // console log vil logge til chrome console
-    return Array.from(
-      document.querySelectorAll("div.search-result-list h3 a")
-    ).map((x) => {
-      console.log(x);
-      return x.href;
-    });
-  });
-  console.groupEnd()
-  
-  console.log(jobAdLinksEVALUATE);
-  
-  // console.group('jobAdLinks');
-  // const jobAdLinks = await page.$$eval("div.search-result-list h3 a", (e) => {
-  //   console.log(e);
-  //   return e;
+  // console.group('jobAdLinksEVALUATE');
+  // const jobAdLinksEVALUATE = await page.evaluate(() => {
+  //   // evaluate er generic for generell, catch all, client side js
+  //   // console log vil logge til chrome console
+  //   return Array.from(
+  //     document.querySelectorAll("div.search-result-list h3 a")
+  //   ).map((x) => {
+  //     console.log(x);
+  //     return x.href;
+  //   });
   // });
-  // console.groupEnd();
-  // console.log('woaaaaaaahaha');
+  // console.groupEnd()
+
+  // console.log(jobAdLinksEVALUATE);
+
+  console.group("jobAdLinks");
+  const jobAdLinks = await page.$$eval("div.search-result-list h3 a", (e) => {
+    console.log(e);
+    return e.map((i) => i.href);
+  });
+  console.groupEnd();
+
+  console.log(jobAdLinks[0]);
 
   // console.log(jobAdLinks);
 
   // console.log('dingenligngeling');
 
-  await page.goto(jobAdLinksEVALUATE[0]);
-  await page.screenshot({ path: "screenshots/job.png"})
+  // await page.goto(jobAdLinksEVALUATE[0]);
+  await page.goto(jobAdLinks[0]);
+  await page.screenshot({ path: "screenshots/job.png" });
 
   // create file from job advertisement page
   // createFile(page);
 
-  console.log('end');
+  console.log("end");
   await browser.close();
 };
 
